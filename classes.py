c51308d340cdef4ef420c617e75378207f017cd0
David Vasquez <david@pop-os.localdomain>
2021-07-16 15:27:43 -0400

    Agrego clases faltantes (columnas pendientes)

--- classes.py
+++ classes.py
@@ -23,6 +23,29 @@ class Picture(Base):
     picture_bytes = sqlalchemy.Column(sqlalchemy.dialects.mysql.LONGBLOB)
     face_bytes = sqlalchemy.Column(sqlalchemy.dialects.mysql.LONGBLOB)
 
+class Employee(Base):
+    __tablename__ = 'empleado'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    first_name = sqlalchemy.Column(sqlalchemy.String(length=40))
+    last_name = sqlalchemy.Column(sqlalchemy.String(length=50))
+    cedula = sqlalchemy.Column(sqlalchemy.String(length=13))
+    vacations_since = sqlalchemy.Column(sqlalchemy.DateTime) #tambien hacer una de tiempo de licencia
+    active = sqlalchemy.Column(sqlalchemy.Boolean) 
+    #Sintomas y lo demas referente a enfermedades y/o covid pendiente
+
+class Tarjeta_vacuna(Base):
+    __tablename__ = 'vacuna'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    cedula_employee = sqlalchemy.Column(sqlalchemy.Integer)
+    first_dose_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    second_dose_date = sqlalchemy.Column(sqlalchemy.DateTime)
+
+class Poncheo(Base):
+    __tablename__ = 'poncheo'
+    id_empleado = sqlalchemy.Column(sqlalchemy.Integer)
+    action = sqlalchemy.Column(sqlalchemy.String(length=7)) #entrada o salida
+    time_action = sqlalchemy.Column(sqlalchemy.DateTime) #sea entrada o salida, tendra la hora de este
+
 
 if __name__ == '__main__':
     #when runned as file, it'll to create all clases as tables on the database
